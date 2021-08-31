baf366de4685fd52e4602334f43808b06ebcbc8a
Ola Einvik <einvik@gmail.com>
2021-08-31 14:59:31 +0200

    test synth

--- src/Android/AzureSpeech.java
+++ src/Android/AzureSpeech.java
@@ -1,7 +1,12 @@
 package com.einvik.cordova.plugin.azurespeech;
-// The native Toast API
-import android.widget.Toast;
-// Cordova-required packages
+
+import com.microsoft.cognitiveservices.speech.AudioDataStream;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesizer;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesisOutputFormat;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesisResult;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -13,25 +18,38 @@ public class AzureSpeech extends CordovaPlugin {
   public boolean execute(String action, JSONArray args,
     final CallbackContext callbackContext) {
       // Verify that the user sent a 'show' action
-      if (!action.equals("test")) {
-        callbackContext.error("\"" + action + "\" is not a recognized action.");
-        return false;
+      switch(action) {
+          case "synthesise":
+            this.Synthesize(args);
+          break;
+          default: 
+            callbackContext.error("\"" + action + "\" is not a recognized action.");
+            return false;
       }
-      String message;
-      try {
-        JSONObject options = args.getJSONObject(0);
-        message = options.getString("message");
-      } catch (JSONException e) {
-        callbackContext.error("Error encountered: " + e.getMessage());
-        return false;
-      }
-      // Create the toast
-      Toast toast = Toast.makeText(cordova.getActivity(), message, Toast.LENGTH_LONG);
-      // Display toast
-      toast.show();
+
+
+
+    //   String message;
+    //   try {
+    //     JSONObject options = args.getJSONObject(0);
+    //     message = options.getString("message");
+    //   } catch (JSONException e) {
+    //     callbackContext.error("Error encountered: " + e.getMessage());
+    //     return false;
+    //   }
+
       // Send a positive result to the callbackContext
       PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
       callbackContext.sendPluginResult(pluginResult);
       return true;
+  }
+
+  public boolean Synthesize(JSONArray args) {
+        JSONObject options = args.getJSONObject(0);
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription(options.getString("SubscriptionKey"),options.getString("ServiceRegion") );
+         AudioConfig audioConfig = AudioConfig.fromDefaultSpeakerOutput();
+
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer(speechConfig, audioConfig);
+        synthesizer.SpeakText(options.getString("Message"));
   }
 }
