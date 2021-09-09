05470c74e4f9e51f4ddd9dc097c8fcb0928437a4
Ola Einvik <einvik@gmail.com>
2021-09-09 09:24:00 +0200

    test

--- src/ios/AzureSpeech.m
+++ src/ios/AzureSpeech.m
@@ -8,7 +8,7 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = @"Hei, du har min tillatelse.";
-
+    NSLog(@"%@",echo);
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
