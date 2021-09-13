14dcaeff1e2a0324f627332e3fa772eed0d65866
Ola Einvik <einvik@gmail.com>
2021-09-13 10:04:47 +0200

    TEST

--- src/ios/AzureSpeech.m
+++ src/ios/AzureSpeech.m
@@ -4,66 +4,113 @@
 
 @implementation AzureSpeech
 
-- (void)hasPermission:(CDVInvokedUrlCommand*)command
-{
-      BOOL hasPermission = FALSE;
-    if ([[AVAudioSession sharedInstance] recordPermission] == AVAudioSessionRecordPermissionGranted) {
-      hasPermission = TRUE;
-    }
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:hasPermission];
-    [pluginResult setKeepCallbackAsBool:NO];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
+  - (void)hasPermission:(CDVInvokedUrlCommand*)command
+  {
+        BOOL hasPermission = FALSE;
+      if ([[AVAudioSession sharedInstance] recordPermission] == AVAudioSessionRecordPermissionGranted) {
+        hasPermission = TRUE;
+      }
+      CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:hasPermission];
+      [pluginResult setKeepCallbackAsBool:NO];
+      [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  }
 
-- (void)getPermission:(CDVInvokedUrlCommand*)command 
-{
-  [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL hasPermission) {
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:hasPermission];
-        [pluginResult setKeepCallbackAsBool:NO];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-  }];
-}
+  - (void)getPermission:(CDVInvokedUrlCommand*)command 
+  {
+    [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL hasPermission) {
+          CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:hasPermission];
+          [pluginResult setKeepCallbackAsBool:NO];
+          [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+  }
 
-- (void)synthesize:(CDVInvokedUrlCommand*)command 
-{
-  // NSString *speechKey = @"asdfasdf";
-  // NSString *serviceRegion = @"northeurope";
-  // SPXSpeechConfiguration *speechConfig = [[SPXSpeechConfiguration alloc] initWithSubscription:speechKey region:serviceRegion];
-  // NSString * result = [[command.arguments valueForKey:@"description"] componentsJoinedByString:@""];
-NSString *result = [command.arguments componentsJoinedByString:@","];
-NSLog(@"result = %@", result);
-NSLog(@"result = %@", [result class]);
-NSDictionary* options = command.arguments[0];
-NSLog(@"Message = %@", options[@"Message"]);
+  - (void)synthesize:(CDVInvokedUrlCommand*)command 
+  {
+    NSString *result = [command.arguments componentsJoinedByString:@","];
+    NSDictionary* options = command.arguments[0];
+    NSString *Message = options[@"Message"];
+    NSString *ServiceRegion = options[@"ServiceRegion"];
+    NSString *SubscriptionKey = options[@"SubscriptionKey"];
 
-NSString *Message = options[@"Message"];
-NSString *ServiceRegion = options[@"ServiceRegion"];
-NSString *SubscriptionKey = options[@"SubscriptionKey"];
-
-// id json = [NSJSONSerialization JSONObjectWithData:result options:0 error:nil];
     SPXSpeechConfiguration *speechConfig = [[SPXSpeechConfiguration alloc] initWithSubscription:SubscriptionKey region:ServiceRegion];
     [speechConfig setSpeechSynthesisOutputFormat:SPXSpeechSynthesisOutputFormat_Audio16Khz32KBitRateMonoMp3];
     SPXSpeechSynthesizer *speechSynthesizer = [[SPXSpeechSynthesizer alloc] initWithSpeechConfiguration:speechConfig audioConfiguration:nil];
     SPXSpeechSynthesisResult *speechResult = [speechSynthesizer speakText:Message];
     CDVPluginResult* pluginResult = nil;
-  if (SPXResultReason_Canceled == speechResult.reason) {
+    if (SPXResultReason_Canceled == speechResult.reason) 
+    {
       SPXSpeechSynthesisCancellationDetails *details = [[SPXSpeechSynthesisCancellationDetails alloc] initFromCanceledSynthesisResult:speechResult];
       NSLog(@"Speech synthesis was canceled: %@. Did you pass the correct key/region combination?", details.errorDetails);
       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
 
-    } else if (SPXResultReason_SynthesizingAudioCompleted == speechResult.reason) {
+    } 
+    else if (SPXResultReason_SynthesizingAudioCompleted == speechResult.reason) 
+    {
         NSLog(@"Speech synthesis was completed");
         self.player = [[AVAudioPlayer alloc] initWithData:[speechResult audioData] error:nil];
         [self.player prepareToPlay];
         [self.player play];
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:YES];
-
-    } else {
+    } 
+    else 
+    {
         NSLog(@"There was an error.");
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:NO];
     }
     [pluginResult setKeepCallbackAsBool:NO];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+  }
+
+  - (void)recognizeFromMicrophone {
+    SPXSpeechConfiguration *speechConfig = [[SPXSpeechConfiguration alloc] initWithSubscription:speechKey region:serviceRegion];
+    if (!speechConfig) {
+        NSLog(@"Could not load speech config");
+        [self updateRecognitionErrorText:(@"Speech Config Error")];
+        return;
+    }
+
+    SPXSpeechRecognizer* speechRecognizer = [[SPXSpeechRecognizer alloc] init:speechConfig];
+    if (!speechRecognizer) {
+        NSLog(@"Could not create speech recognizer");
+        // [self updateRecognitionResultText:(@"Speech Recognition Error")];
+        return;
+    }
+    // connect callbacks
+    [speechRecognizer addRecognizingEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs) {
+        NSLog(@"Received intermediate result event. SessionId: %@, recognition result:%@. Status %ld. offset %llu duration %llu resultid:%@", eventArgs.sessionId, eventArgs.result.text, (long)eventArgs.result.reason, eventArgs.result.offset, eventArgs.result.duration, eventArgs.result.resultId);
+        // [self updateRecognitionStatusText:eventArgs.result.text];
+    }];
+
+    [speechRecognizer addRecognizedEventHandler: ^ (SPXSpeechRecognizer *recognizer, SPXSpeechRecognitionEventArgs *eventArgs) {
+        NSLog(@"Received final result event. SessionId: %@, recognition result:%@. Status %ld. offset %llu duration %llu resultid:%@", eventArgs.sessionId, eventArgs.result.text, (long)eventArgs.result.reason, eventArgs.result.offset, eventArgs.result.duration, eventArgs.result.resultId);
+        // [self updateRecognitionResultText:eventArgs.result.text];
+    }];
+
+    // session stopped callback to recognize stream has ended
+    __block bool end = false;
+    [speechRecognizer addSessionStoppedEventHandler: ^ (SPXRecognizer *recognizer, SPXSessionEventArgs *eventArgs) {
+        NSLog(@"Received session stopped event. SessionId: %@", eventArgs.sessionId);
+        end = true;
+    }];
+
+    [speechRecognizer startContinuousRecognition];
+    while (end == false)
+        [NSThread sleepForTimeInterval:1.0f];
+    [speechRecognizer stopContinuousRecognition];
+    // SPXSpeechRecognitionResult *speechResult = [speechRecognizer recognizeOnce];
+    // if (SPXResultReason_Canceled == speechResult.reason) {
+    //     SPXCancellationDetails *details = [[SPXCancellationDetails alloc] initFromCanceledRecognitionResult:speechResult];
+    //     NSLog(@"Speech recognition was canceled: %@. Did you pass the correct key/region combination?", details.errorDetails);
+    //     [self updateRecognitionErrorText:([NSString stringWithFormat:@"Canceled: %@", details.errorDetails ])];
+    // } else if (SPXResultReason_RecognizedSpeech == speechResult.reason) {
+    //     NSLog(@"Speech recognition result received: %@", speechResult.text);
+    //     [self updateRecognitionResultText:(speechResult.text)];
+    // } else {
+    //     NSLog(@"There was an error.");
+    //     [self updateRecognitionErrorText:(@"Speech Recognition Error")];
+    // }
+
+
 }
 
 @end
